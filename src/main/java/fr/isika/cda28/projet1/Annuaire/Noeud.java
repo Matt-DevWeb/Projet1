6d3902e4729a6e293a3b8f5c1b7bf503f8d1c866
Manel Merini <myloviche15@gmail.com>
2024-11-07 09:35:48 +0100

    debug lire noeud

--- src/main/java/fr/isika/cda28/projet1/Annuaire/Noeud.java
+++ src/main/java/fr/isika/cda28/projet1/Annuaire/Noeud.java
@@ -71,12 +71,12 @@ public class Noeud extends Annuaire {
 //	}
 //	
 	public void ajoutStagiaireRecursif(Noeud nouveauNoeud, RandomAccessFile raf) throws IOException {
-		if (this.stagiaire.getNomLong().compareTo(nouveauNoeud.getStagiaire().getNomLong()) < 0) {
+		if (this.stagiaire.getNomLong().compareTo(nouveauNoeud.getStagiaire().getNomLong()) > 0) {
 			if (this.filsGauche == -1) {
-				raf.seek(raf.getFilePointer()-8);
-				raf.writeInt((int)raf.length()/TAILLE_NOEUD_OCTET);
-				raf.seek(raf.length());
-				ecrireNoeud(nouveauNoeud, filsGauche, filsDroit);
+				raf.seek(raf.getFilePointer()-8);// on repositionne pour ecrire l'index
+				raf.writeInt((int)raf.length()/TAILLE_NOEUD_OCTET); // on ecrit l'index dans le parent 
+				raf.seek(raf.length());// retour a la fin 
+				ecrireNoeud(nouveauNoeud, -1, -1);// ecrire le nouveau noeud (fils gauche)
 			} else {
 				raf.seek(this.filsGauche*TAILLE_NOEUD_OCTET);
 				Noeud noeudFilsGauche = lireNoeud(nouveauNoeud, -1, -1);
@@ -88,7 +88,7 @@ public class Noeud extends Annuaire {
 				raf.seek(raf.getFilePointer()-4);
 				raf.writeInt((int)raf.length()/TAILLE_NOEUD_OCTET);
 				raf.seek(raf.length());
-				ecrireNoeud(nouveauNoeud, filsGauche, filsDroit);
+				ecrireNoeud(nouveauNoeud, -1, -1);
 			} else {
 				raf.seek(this.filsDroit*TAILLE_NOEUD_OCTET);
 				Noeud noeudFilsDroit = lireNoeud(nouveauNoeud, -1, -1);
